a741cfcda263ed030353a31235114552d5b6858c
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2022-06-09 14:29:48 +0200

    Update wikitest.sh

--- Linux/wikitest.sh
+++ Linux/wikitest.sh
@@ -1,10 +1,12 @@
 # Demonstriert die select-Anweisung
 # aselect1
+
+aws="AWS ist ein US-Unternehmen im Cloud-Computing Bereich. In dieser Branche ist das Unternehmen sogar Weltmarktführer"
 select auswahl in AWS SecretManager S3 bucket
 do
    echo "Ihre Auswahl war : $auswahl"
    if auswahl="AWS" 
    then 
-        echo "AWS ist ein US-Unternehmen"
+        echo $aws
     fi
 done
