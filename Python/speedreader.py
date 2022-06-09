e2a4401cd65b0dda9ce688bad409d614a4bffa17
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2022-06-09 14:11:29 +0200

    new code

--- Python/speedreader.py
+++ Python/speedreader.py
@@ -1,6 +1,6 @@
-wert = 3
+wert = 9
 if wert < 5:
     print('Wert ist kleiner als 5')
-    print('Ich gehöre auch noch zu der Bedingung')
     
-print('und hier geht es nach der if-Abfrage weiter')
+if wert > 4:
+    print('Wert ist größer als 4')
