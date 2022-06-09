86baf9c1548b16b4c785dc5829a2f30e53bfb3c9
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2022-06-09 14:10:10 +0200

    new code

--- Python/speedreader.py
+++ Python/speedreader.py
@@ -1,33 +1,6 @@
-def greet(name):
-    """
-    This function greets to
-    the person passed in as
-    a parameter
-    """
-    print("Hello, " + name + ". Good morning!")
-
-greet('Paul')
-
-
-def absolute_value(num):
-    """This function returns the absolute
-    value of the entered number"""
-
-    if num >= 0:
-        return num
-    else:
-        return -num
-
-
-print(absolute_value(2))
-
-print(absolute_value(-4))
-
-# Python Module example
-
-def add(a, b):
-   """This program adds two
-   numbers and return the result"""
-
-   result = a + b
-   return result
+wert = 3
+if wert < 5:
+    print('Wert ist kleiner als 5')
+    print('Ich gehöre auch noch zu der Bedingung')
+    
+print('und hier geht es nach der if-Abfrage weiter')
