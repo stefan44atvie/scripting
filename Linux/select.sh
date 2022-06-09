d5a117a1c027d5c9cc441747f835f737aa565cfa
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2022-06-09 14:47:34 +0200

    new

--- Linux/select.sh
+++ Linux/select.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# elif statements
-if [ $1 -ge 18 ]
-then
-echo You may go to the party.
-elif [ $2 == 'yes' ]
-then
-echo You may go to the party but be back before midnight.
-else
-echo You may not go to the party.
-fi
+# case example
+case $1 in
+start)
+echo starting
+;;
+stop)
+echo stoping
+;;
+restart)
+echo restarting
+;;
+*)
+echo don\'t know
+;;
+esac
