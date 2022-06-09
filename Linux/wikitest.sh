db204de727f6fdf1ac24e90a8c69463c010beffd
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2022-06-09 14:36:47 +0200

    if

--- Linux/wikitest.sh
+++ Linux/wikitest.sh
@@ -5,7 +5,7 @@ S3="ein bucket online speicher"
 aws="AWS ist ein US-Unternehmen im Cloud-Computing Bereich. In dieser Branche ist das Unternehmen sogar Weltmarktführer"
 select auswahl in AWS SecretManager S3 bucket
 do
-   #echo "Ihre Auswahl war : $auswahl"
+   echo "Ihre Auswahl war : $auswahl"
    if auswahl="AWS" 
    then 
         echo $aws
