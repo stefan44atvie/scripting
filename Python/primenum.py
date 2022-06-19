b4d5c7f16ae4c5566a51353169c44c2bfd0d569f
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2022-06-19 16:14:24 +0200

    new code

--- Python/primenum.py
+++ Python/primenum.py
@@ -1,4 +1,7 @@
 import time
+datei = open('results.txt','r')
+print(datei.read())
+
 def is_prime(n):
     if n <= 1:
         return False
@@ -6,12 +9,16 @@ def is_prime(n):
         if n % i == 0:
             return False
     print(n)
+    file1=open("results.txt","a")
+    str1=repr(n)
+    file1.write(str1+"\n")
+    file1.close
     return True
  
 t0 = time.time()
 c = 0
  
-for n in range(1,100):
+for n in range(1,250):
     x = is_prime(n)
     c += x
 print("Total prime numbers in range :", c)
